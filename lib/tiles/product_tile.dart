7ee3fc59757dbefbe79f6d8984e23136aa077b56
Joao Paulo Faustino Santos <jpaulofsantos@gmail.com>
2021-03-12 08:04:04 -0300

    concluding product tile with grid and list views

--- lib/tiles/product_tile.dart
+++ lib/tiles/product_tile.dart
@@ -11,6 +11,77 @@ class ProductTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return InkWell( //ao tocar cria uma pequena anição, é semelhante ao gesture detector
+      child: Card(
+        child: type == "grid" ? //se for Grid, insere uma coluna, se for list, insere uma linha
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch, //fornece uma imagem esticada
+          mainAxisAlignment: MainAxisAlignment.start, //alinhando
+          children: [
+            AspectRatio( //razao entre largura e altura da imagem
+                aspectRatio: 0.8,
+              child: Image.network(productData.images[0],
+                fit: BoxFit.cover, //imagem cobrindo o tamanho do aspectratio
+              ),
+            ),
+            Expanded( //utiliza o espaço disponivel
+                child: Container(
+                  padding: EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      Text(productData.title,
+                        style: TextStyle(
+                          fontWeight: FontWeight.w500
+                        ),
+                      ),
+                      Text("R\$ ${productData.price.toStringAsFixed(2)}",
+                        style: TextStyle(
+                          color: Theme.of(context).primaryColor,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 17.0
+                        ),
+                      )
+                    ],
+                  ),
+                )
+            )
+          ],
+        )
+            : Row(
+          children: [
+            Flexible(
+              flex: 1, //aplica uma divisão de espação da linha igualmente (50%)
+                child: Image.network(productData.images[0],
+                fit: BoxFit.cover,
+                  height: 250.0,
+                )
+            ),
+            Flexible(
+              flex: 1, //aplica uma divisão de espação da linha igualmente (50%). Ex: caso seja 8 e o outro flex seja 2, este será 4x maior
+                child: Container(
+                  padding: EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(productData.title,
+                        style: TextStyle(
+                            fontWeight: FontWeight.w500
+                        ),
+                      ),
+                      Text("R\$ ${productData.price.toStringAsFixed(2)}",
+                        style: TextStyle(
+                            color: Theme.of(context).primaryColor,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 17.0
+                        ),
+                      )
+                    ],
+                  ),
+                )
+            )
+          ],
+        )
+      ),
+    );
   }
 }
